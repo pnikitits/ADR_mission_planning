3464647c533434a3084595615682be81c863ad13
pnikitits <112090831+pnikitits@users.noreply.github.com>
2024-03-14 10:21:41 +0100

    Update State

--- State.py
+++ State.py
@@ -11,16 +11,15 @@ class State:
         self.current_removing_debris = first_debris # Index
         self.binary_flags = np.zeros(total_n_debris).tolist()
         self.binary_flags[self.current_removing_debris] = 1
+        self.priority_list = np.ones(total_n_debris).tolist()
 
-    def transition_function(self , action ,  cv , dt_min , debug = False): # Looks like it works
+    def transition_function(self , action ,  cv , dt_min , priority_debris, debug = True): # Looks like it works
         
-        
-
         self.removal_step += 1
         self.number_debris_left -= 1
         self.dt_left -= action[1] # NOT dt_min ?
         
-        print(f"--- Taking action {action}: 'dv={cv} , dt={dt_min}") if debug else None
+        print(f"--- Taking action {action}: 'dv={cv} , dt={dt_min}")
 
         # print(f"{otv}")
         # print(f"{target}")
@@ -30,12 +29,28 @@ class State:
         self.current_removing_debris = action[0]
         self.binary_flags[self.current_removing_debris] = 1
 
-
-
+        # Add a higher priority to the selected debris
+        if priority_debris != None:
+            self.priority_list[priority_debris] = 10
+            print(f"Priority given to {priority_debris}")
 
 
     def to_list(self):
-        return [self.removal_step , self.number_debris_left , self.dv_left , self.dt_left , self.current_removing_debris] + self.binary_flags
+        # Toggle to give access to the priority list
+        access_priority_list = True
+        # create a list of zeros same lenght as the priority list
+        p_list = np.zeros(len(self.priority_list)).tolist()
+        if access_priority_list:
+            p_list = self.priority_list
+
+        print(f"Priority list: {p_list}")
+        print(f"Binary flags: {self.binary_flags}")
+
+        return [self.removal_step , 
+                self.number_debris_left , 
+                self.dv_left ,
+                self.dt_left , 
+                self.current_removing_debris] + self.binary_flags + p_list
         
 
 

a418f52d294baaf4b02cc7366fcf60750010952c
hugodrw <hugoderohanwillner@gmail.com>
2024-04-03 08:56:07 +0200

    testing softmax to greedy

--- src/agent/pytorch_agent.py
+++ src/agent/pytorch_agent.py
@@ -53,7 +53,7 @@ class Agent(BaseAgent):
         """
         state = torch.tensor(state, device = self.device, dtype = torch.float32)
         # action = self.policy_network.select_action(state, self.tau)
-        # action = self.policy_network.softmax_to_greedy_action(state, self.tau)
+        action = self.policy_network.softmax_to_greedy_action(state, self.tau)
         # action = self.policy_network.select_greedy_action(state)
         
         return action
